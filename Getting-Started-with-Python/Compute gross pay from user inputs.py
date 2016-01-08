bddbe5331d3a3d749347f2479a1a299da13c484b
Jason Shipp <jason.shipp@uswitch.com>
2016-01-08 13:34:42 +0000

    Create Compute gross pay from user inputs.py

---
+++ Getting-Started-with-Python/Compute gross pay from user inputs.py
@@ -0,0 +1,8 @@
+'''
+Write a program to prompt the user for hours and rate per hour, and compute gross pay. 
+'''
+
+hrs = float(raw_input("Enter Hours:"))
+rate = float(raw_input("Enter Rate:"))
+
+print hrs * rate
